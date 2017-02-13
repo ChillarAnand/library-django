c6ae7f80d90cc553ae8799207277aa75de1ce8d3
chillaranand <anand21nanda@gmail.com>
2017-02-13 21:18:40 +0530

    Refactored settings

---
+++ library/settings/dev.py
@@ -0,0 +1,13 @@
+from .base import *
+
+
+SECRET_KEY = 'q6emagfzaeftr*4$o@@608v3!)(^cmvwm@2kcatu7if(c#0w+@'
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['0.0.0.0', ]
+
+
+INSTALLED_APPS += (
+    'django_extensions',
+)
