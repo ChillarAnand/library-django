77a2d49b6bb1596e69ce732c74ed9cb65ef5873f
Chillar Anand <anand21nanda@gmail.com>
2019-07-29 21:04:21 +0530

    Cleanup admin

--- book/admin.py
+++ book/admin.py
@@ -12,30 +12,29 @@ from crm.models import House
 
 
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('name', 'author', 'delete', )
     search_fields = ('name',)
-    list_display = ('name', 'author_link', 'name', 'name')
-    list_display = ('name', 'author', 'name', 'name')
-    list_display = ('name', 'author', 'borrowed')
-    list_display = ('name', 'author', 'borrowed', 'name', 'name')
+    # autocomplete_fields = ['author']
 
-    list_display_links = ('name', 'author',)
+    list_display = ('name', 'author', 'delete', )
+    list_display = ('name', 'author', 'author_link', 'delete', 'borrowed', 'is_available', 'name', 'name')
+
+    # list_display_links = ('name', 'author',)
 
     def author_link(self, book):
         link = reverse("admin:book_author_change", args=[book.author.id])
-        return u'<a href="%s">%s</a>' % (link, book.author.name)
-    author_link.allow_tags = True
+        html = '<a href="{}">{}</a>'.format(link, book.author.name)
+        return format_html(html)
     author_link.short_description = 'Author'
-
-    # autocomplete_fields = ['author']
 
     def delete(self, book):
         link = reverse("admin:book_book_delete", args=[book.pk])
-        button = '<input type="button" onclick="location.href=\'{}\'" value="Delete" />'
-        return button.format(link)
-    delete.allow_tags = True
-    delete.short_description = 'Delete book'
+        html = '<input type="button" onclick="location.href=\'{}\'" value="Delete" />'.format(link)
+        return format_html(html)
+    delete.short_description = 'Delete'
 
+    def toggle_availability(self, book):
+        html = ''
+        return format_html(html)
 
 class AuthorAdmin(admin.ModelAdmin):
     search_fields = ('name',)
