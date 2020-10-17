be6993facf919f57ad2c61a25bb39cadb46bc820
Chillar Anand <anand21nanda@gmail.com>
2020-10-17 19:35:34 +0530

    Added environment to settings

--- library/settings.py
+++ library/settings.py
@@ -325,6 +325,8 @@ from sentry_sdk.integrations.django import DjangoIntegration
 
 SENTRY_DSN = os.environ.get('SENTRY_DSN')
 VERSION = os.environ.get('VERSION')
+ENVIRONMENT = os.environ.get('ENVIRONMENT')
+
 if SENTRY_DSN:
     sentry_sdk.init(
         dsn=SENTRY_DSN,
@@ -332,4 +334,5 @@ if SENTRY_DSN:
         traces_sample_rate=1.0,
         send_default_pii=True,
         release=VERSION,
+        environment=ENVIRONMENT,
     )
