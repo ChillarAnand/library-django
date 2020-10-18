568f05b2ab994fda6f9fefcfbc35bf81dc40a388
Chillar Anand <anand21nanda@gmail.com>
2020-10-18 15:24:33 +0530

    Added setup file

--- library/__init__.py
+++ library/__init__.py
@@ -1,1 +1,3 @@
 from library.checks import *  # noqa
+
+VERSION = '1.1.3'
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+
+setup(
+    name='library',
+    version='1.1.3',
+    packages=['library'],
+)
