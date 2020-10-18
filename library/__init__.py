9efc109584f88b61c43547125bb38a5d12b1e3dc
Chillar Anand <anand21nanda@gmail.com>
2020-10-18 15:25:52 +0530

    Bump version: 1.1.4 → 1.1.5

--- library/__init__.py
+++ library/__init__.py
@@ -1,3 +1,3 @@
 from library.checks import *  # noqa
 
-VERSION = '1.1.4'
+VERSION = '1.1.5'
--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 
 setup(
     name='library',
-    version='1.1.4',
+    version='1.1.5',
     packages=['library'],
 )
